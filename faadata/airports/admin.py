a16a134819f282bfb906214143bec2b34b5b2452
Adam Fast <adam@adamfast.com>
2010-12-02 22:49:31 -0600

    Add searching to airport admin.

--- faadata/airports/admin.py
+++ faadata/airports/admin.py
@@ -4,5 +4,6 @@ from faadata.airports.models import Airport
 class AirportAdmin(admin.OSMGeoAdmin):
 #class AirportAdmin(admin.ModelAdmin):
     list_display = ('location_identifier', 'facility_name', 'elevation_msl', 'status')
+    search_fields = ('location_identifier', 'icao_identifier', 'facility_name')
 
 admin.site.register(Airport, AirportAdmin)
