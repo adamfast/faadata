b94e1b0f1df6d88acde425af6a9da91d16406bd9
Adam Fast <adam@adamfast.com>
2010-12-20 18:23:11 -0600

    Add (insanely) basic airport detail view

--- faadata/airports/urls.py
+++ faadata/airports/urls.py
@@ -6,5 +6,6 @@ from faadata.airports.models import *
 airport_list = Airport.objects.all().order_by('location_identifier')
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>[a-zA-Z0-9*.]+)/$', 'django.views.generic.list_detail.object_detail', {'queryset': airport_list, 'slug_field': 'location_identifier'}, name='airport_detail'),
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': airport_list, 'paginate_by': getattr(settings, 'AIRPORTS_PAGINATE_BY', 20)}),
 )
