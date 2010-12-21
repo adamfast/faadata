25c890c8d0c748d0da3ad2703671980d9b088b51
Adam Fast <adam@adamfast.com>
2010-12-20 18:28:42 -0600

    Add a (very) basic fix detail template.

--- faadata/fixes/models.py
+++ faadata/fixes/models.py
@@ -1,4 +1,5 @@
 from django.contrib.gis.db import models
+from django.core.urlresolvers import reverse
 
 class Fix(models.Model):
     id = models.CharField(max_length=8, primary_key=True)
@@ -6,6 +7,9 @@ class Fix(models.Model):
     artcc = models.CharField(max_length=4, db_index=True)
     state_post_office_code = models.CharField(max_length=2)
     fix_type = models.CharField(max_length=8, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('fix_detail', args=[self.id])
 
     def __unicode__(self):
         return self.id
--- faadata/fixes/urls.py
+++ faadata/fixes/urls.py
@@ -6,5 +6,6 @@ from faadata.fixes.models import *
 fix_list = Fix.objects.all().order_by('id')
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>[a-zA-Z0-9*.]+)/$', 'django.views.generic.list_detail.object_detail', {'queryset': fix_list, 'slug_field': 'id'}, name='fix_detail'),
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': fix_list, 'paginate_by': getattr(settings, 'FIXES_PAGINATE_BY', 20)}),
 )
