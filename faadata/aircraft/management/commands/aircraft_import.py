e316db8c8cacae5cedd2f805a28583fd4207f3bd
Adam Fast <adam@adamfast.com>
2011-06-02 21:39:09 -0500

    Add a management command to handle automatic import of the FAA Aircraft registration database.

---
+++ faadata/aircraft/management/commands/aircraft_import.py
@@ -0,0 +1,25 @@
+import datetime
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from faadata.aircraft.load import import_aircraftregistration, import_aircraftmanufacturercodes
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--path', default='', dest='path',
+            help='The directory where the aircraft data is stored.'),
+    )
+    help = ("Imports data from the FAA aircraft data download.")
+
+    def handle(self, *args, **options):
+        if settings.DEBUG:
+            print('You really should turn settings.DEBUG off, or else this script will eat a very large amount of your RAM.')
+        else:
+            input_path = options['path']
+
+            start_time = datetime.datetime.now()
+            import_aircraftregistration(input_path)
+            import_aircraftmanufacturercodes(input_path)
+            print('Took %s' % (datetime.datetime.now() - start_time))
