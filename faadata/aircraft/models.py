16d45331ebc1b626e6c6943a0169b989a31a1c21
Adam Fast <adam@adamfast.com>
2012-03-12 16:29:49 -0500

    Adding status choices to match the FAA documentation, and allow filtering by them in the admin.

--- faadata/aircraft/models.py
+++ faadata/aircraft/models.py
@@ -79,6 +79,45 @@ BUILDER_CERTIFICATION_CODES = (
     (2, 'Light Sport'),
 )
 
+AIRCRAFT_STATUS_OPTIONS = (
+    # per ARData.pdf in the aircraft registration download
+    ('A', 'The Triennial Aircraft Registration form was mailed and has not been returned by the Post Office'),
+    ('D', 'Expired Dealer'),
+    ('E', 'The Certificate of Aircraft Registration was revoked by enforcement action'),
+    ('M', 'Aircraft registered to the manufacturer under their Dealer Certificate'),
+    ('N', 'Non-citizen Corporations which have not returned their flight hour reports'),
+    ('R', 'Registration pending'),
+    ('S', 'Second Triennial Aircraft Registration Form has been mailed'),
+    ('T', 'Valid Registration from a Trainee'),
+    ('V', 'Valid Registration'),
+    ('X', 'Enforcement Letter'),
+    ('Z', 'Permanent Reserved'),
+    ('1', 'Triennial Aircraft Registration form was returned by the Post Office as undeliverable'),
+    ('2', 'N-Number Assigned - but has not yet been registered'),
+    ('3', 'N-Number assigned as a Non Type Certificated aircraft - but has not yet been registered'),
+    ('4', 'N-Number assigned as import - but has not yet been registered'),
+    ('5', 'Reserved N-Number'),
+    ('6', 'Administratively canceled'),
+    ('7', 'Sale reported'),
+    ('8', 'A second attempt has been made at mailing a Triennial Aircraft Registration form to the owner with no response'),
+    ('9', 'Certificate of Registration has been revoked'),
+    ('10', 'N-Number assigned, has not been registered and is pending cancellation'),
+    ('11', 'N-Number assigned as a Non Type Certificated (Amateur) but has not been registered that is pending cancellation'),
+    ('12', 'N-Number assigned as import but has not been registered that is pending cancellation'),
+    ('13', 'Registration Expired'),
+    ('14', 'First Notice for Re-Registration/Renewal'),
+    ('15', 'Second Notice for Re-Registration/Renewal'),
+    ('16', 'Registration Expired - Pending Cancellation'),
+    ('17', 'Sale Reported - Pending Cancellation'),
+    ('18', 'Sale Reported - Canceled'),
+    ('19', 'Registration Pending - Pending Cancellation'),
+    ('20', 'Registration Pending - Canceled'),
+    ('21', 'Revoked - Pending Cancellation'),
+    ('22', 'Revoked - Canceled'),
+    ('23', 'Expired Dealer (Pending Cancellation)'),
+    ('24', 'Third Notice for Re-Registration/Renewal'),
+)
+
 
 class AircraftManufacturerCode(models.Model):
     code = models.CharField(max_length=8, db_index=True)
@@ -121,7 +160,7 @@ class AircraftRegistration(models.Model):
     approved_operation_codes = models.CharField(max_length=9, null=True, blank=True)
     aircraft_type = models.IntegerField(choices=AIRCRAFT_TYPE, null=True, blank=True)
     engine_type = models.IntegerField(choices=ENGINE_TYPE, null=True, blank=True)
-    status_code = models.CharField(max_length=1, null=True, blank=True) # need options
+    status_code = models.CharField(max_length=2, null=True, blank=True, choices=AIRCRAFT_STATUS_OPTIONS)
     mode_s_code = models.CharField(max_length=8, null=True, blank=True)
     fractional_ownership = models.CharField(max_length=1, null=True, blank=True)
     airworthiness_date = models.DateField(null=True, blank=True)
